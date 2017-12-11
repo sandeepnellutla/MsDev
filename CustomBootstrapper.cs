f444aff7c19f457f42d8f74e3dde0958b580c043
snellutlapv <snellutla@practicevelocity.com>
2017-12-11 11:46:39 -0600

    Initial commit

---
+++ CustomBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Json;
+using Nancy.TinyIoc;
+
+namespace MSDev
+{
+    public class CustomBootstrapper:DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            JsonSettings.MaxJsonLength = Int32.MaxValue;
+        }
+    }
+}
